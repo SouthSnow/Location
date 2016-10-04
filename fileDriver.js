7f3e5d2277450e376a35360483339220b12b6cb5
SouthSnow <pflnh@163.com>
2016-10-03 22:04:38 -0400

    tijiao

--- fileDriver.js
+++ fileDriver.js
@@ -31,17 +31,24 @@ FileDriver.prototype.handleGet = function(req, res) { //1
     var fileId = req.params.id;
     if (fileId) {
         this.get(fileId, function(error, thisFile) { //2
-            if (error) { res.send(400, error); }
+            if (error) { 
+                res.send(error);
+                res.status(404);
+             }
             else {
                     if (thisFile) {
                          var filename = fileId + thisFile.ext; //3
                          var filePath = './uploads/'+ filename; //4
     	                 res.sendfile(filePath); //5
-    	            } else res.send(404, 'file not found');
+    	            } else {
+                     res.send('file not found');
+                     res.status(404);
+                  }
             }
         });        
     } else {
-	    res.send(404, 'file not found');
+	    res.send('file not found');
+      res.status(404);
     }
 }
 
@@ -70,18 +77,23 @@ FileDriver.prototype.handleUploadRequest = function(req, res) { //1
     var ext = ctype.substr(ctype.indexOf('/')+1); //3
     if (ext) {ext = '.' + ext; } else {ext = ''};
     this.getNewFileId({'content-type':ctype, 'ext':ext}, function(err,id) { //4
-        if (err) { res.send(400, err); } 
+        if (err) { 
+          res.send(error);
+          res.status(404);
+         } 
         else { 	         
              var filename = id + ext; //5
              filePath = __dirname + '/uploads/' + filename; //6
  
-	     var writable = fs.createWriteStream(filePath); //7
-	     req.pipe(writable); //8
+	           var writable = fs.createWriteStream(filePath); //7
+	           req.pipe(writable); //8
              req.on('end', function (){ //9
-               res.send(201,{'_id':id});
+                res.send({'_id':id});
+                res.status(201);
              });               
              writable.on('error', function(err) { //10
-                res.send(500,err);
+                res.send(err);
+                res.status(500);
              });
         }
     });
