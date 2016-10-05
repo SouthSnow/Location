d6de2aaa01c87272d7cda28e7ccb92d8a96c6986
pangfuli <pflnh@me.com>
2016-10-05 10:39:16 +0800

    ti

--- fileDriver.js
+++ fileDriver.js
@@ -1,5 +1,6 @@
 var ObjectID = require('mongodb').ObjectID
-  , fs = require('fs'); //1
+  , fs = require('fs')
+  , formidable = require('formidable'); //1
  
 FileDriver = function(db) { //2
   this.db = db;
@@ -110,12 +111,8 @@ FileDriver.prototype.handleUploadRequest = function(req, res) { //1
              // res.status(201).send({'_id':id});
 
 	           
-
-             var buffers = [];
-             req.on('data', function (data) {
-                buffers.push(data);
-                console.log('About to route a request for req  data: ' + data);
-             });
+             upload(res, req, filePath, id);
+            
 
              //  var writable = fs.createWriteStream(filePath); //7
              // writable.on('pipe', function (src) {
@@ -126,35 +123,42 @@ FileDriver.prototype.handleUploadRequest = function(req, res) { //1
 
 
 
-             req.on('end', function (){ //9
-                console.log('About to route a request for req  end id: ' + id );
-                // var writable = fs.createWriteStream(filePath); //7
-                // buffers.pipe(writable); //8
 
-              // 创建一个可以写入的流，写入到文件 output.txt 中
-              // var writerStream = fs.createWriteStream(filePath);
+             // var buffers = [];
+             // req.on('data', function (data) {
+             //    buffers.push(data);
+             //    console.log('About to route a request for req  data: ' + data);
+             // });
 
-              // // 使用 utf8 编码写入数据
-              // writerStream.write(buffers);
+             // req.on('end', function (){ //9
+             //    console.log('About to route a request for req  end id: ' + id );
+             //    // var writable = fs.createWriteStream(filePath); //7
+             //    // buffers.pipe(writable); //8
 
-              // // 标记文件末尾
-              // writerStream.end();
+             //  // 创建一个可以写入的流，写入到文件 output.txt 中
+             //  // var writerStream = fs.createWriteStream(filePath);
 
-              // // 处理流事件 --> data, end, and error
-              // writerStream.on('finish', function() {
-              //     console.log("写入完成。");
-                 res.status(201).send({'_id':id});
+             //  // // 使用 utf8 编码写入数据
+             //  // writerStream.write(buffers);
 
-              // });
+             //  // // 标记文件末尾
+             //  // writerStream.end();
 
-              // writerStream.on('error', function(err){
-              //    console.log(err.stack);
-                // res.status(404).send("file not find");
-              // });
+             //  // // 处理流事件 --> data, end, and error
+             //  // writerStream.on('finish', function() {
+             //  //     console.log("写入完成。");
+             //     res.status(201).send({'_id':id});
 
-              console.log("程序执行完毕");
+             //  // });
 
-             });  
+             //  // writerStream.on('error', function(err){
+             //  //    console.log(err.stack);
+             //    // res.status(404).send("file not find");
+             //  // });
+
+             //  console.log("程序执行完毕");
+
+             // });  
 
             // writable.on('end', function () {
             //     console.log('something is piping end');
@@ -202,5 +206,29 @@ FileDriver.prototype.handleUploadRequest = function(req, res) { //1
         }
     });
 };
+
+function upload(response, request, filePath, fileId) {
+  console.log("Request handler 'upload' was called")
+
+  var form = new formidable.IncomingForm()
+  form.parse(request, function (error, fields, files) {
+    console.log('parsing done' + files.upload.path)
+
+    if (error) {
+      response.status(404).send('file no find'); 
+      console.log('parsing done' + error)
+    } else {
+      fs.renameSync(files.upload.path, filePath, function (error) {
+        if (error) {
+          fs.unlink(filePath)
+          fs.rename(files.upload.path, filePath)
+        }
+      })
+      response.status(201).send({'_id':fileId});
+    }
+  })
+}
+
+
  
 exports.FileDriver = FileDriver;
