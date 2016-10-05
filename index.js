00911daff32837db3c2f2e3fa54915af49ca157e
pangfuli <pflnh@me.com>
2016-10-05 14:13:17 +0800

    ti

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 app.set('port', process.env.PORT || 3001); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-// app.use(bodyParser());
+app.use(bodyParser());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
