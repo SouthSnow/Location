300ffcb9e78a17fd5bb309922c32a6501eee31fc
pangfuli <pflnh@me.com>
2016-10-05 11:21:41 +0800

    ti

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 // app.use(bodyParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 var mongoHost = 'localhost';
 var mongoPort = 27017;
