b24deec5f7d4c75867217b939c3219b3a785ff67
pangfuli <pflnh@me.com>
2016-10-10 10:44:14 +0800

    提交

--- fileDriver.js
+++ fileDriver.js
@@ -113,14 +113,26 @@ FileDriver.prototype.handleUploadRequest = function(req, res) { //1
              console.log('About to route a request for filePath :' + filePath );
              // res.status(201).send({'_id':id});
 
-             // upload(res, req, filePath, id);
+             upload(res, req, filePath, id);
             
 
-            var writable = fs.createWriteStream(filePath); //7
+            // var writable = fs.createWriteStream(filePath); //7
+            // req.pipe(writable);
+            // writable.on('finish', function () {
+            //     console.log('something is piping finish');
+            //     uploadFile(id);
+            //     res.status(201).send({'_id':id});
+            //  });
+
+            //  writable.on('error', function () {
+            //     console.log('something is piping error');
+            //     uploadFile(id);
+            //     res.status(404).send("file not find");
+            //  });
+
              // writable.on('pipe', function (src) {
              //    console.log('something is piping into the writer');
              //  });
-            req.pipe(writable);
 
 
             // console.log(req.headers);
@@ -171,17 +183,7 @@ FileDriver.prototype.handleUploadRequest = function(req, res) { //1
             //     res.status(201).send({'_id':id});
             //  });
 
-            writable.on('finish', function () {
-                console.log('something is piping finish');
-                uploadFile(id);
-                res.status(201).send({'_id':id});
-             });
-
-             writable.on('error', function () {
-                console.log('something is piping error');
-                uploadFile(id);
-                res.status(404).send("file not find");
-             });
+            
 
                
 
