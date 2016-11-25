88e0d477fc07dec9394078a8f1dba8c3a4006925
pangfuli <pflnh@me.com>
2016-11-25 16:02:52 +0800

    提交

--- fileDriver.js
+++ fileDriver.js
@@ -46,7 +46,7 @@ FileDriver.prototype.handleGet = function(req, res) { //1
             else {
                   if (thisFile) {
                          var filename = fileId + thisFile.ext; //3
-                         var filePath = __dirname +'/uploads/'+ "upload_" + filename; //4
+                         var filePath = __dirname +'/uploads/' + filename; //4
                           fs.exists(filePath, function (exist) {
                              if (!exist) {
                                 filePath =  __dirname +'/uploads/' + 'default.png'
